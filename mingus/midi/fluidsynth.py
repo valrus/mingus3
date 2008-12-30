c63d5e2b924ebfa259214e66a735b5c081d254bb
Rhijnauwen <devnull@localhost>
2008-12-30 05:36:56 +0000

    Replaced fluidsynth and MidiSequencer by the new FluidSynth which makes use of the pyFluidSynth bindings.

--- mingus/midi/fluidsynth.py
+++ mingus/midi/fluidsynth.py
@@ -2,7 +2,7 @@
 
 ================================================================================
 
-	mingus - Music theory Python package, fluidsynth module
+	mingus - Music theory Python package, MIDI sequencer
 	Copyright (C) 2008, Bart Spaans
 
     This program is free software: you can redistribute it and/or modify
@@ -20,58 +20,273 @@
 
 ================================================================================
 
-   fluidsynth is a software MIDI synthesizer which allows you to play the containers 
-   in mingus.containers real-time. To work with this module, you'll 
-   need fluidsynth and a nice instrument collection (look here: http://www.hammersound.net, go to Sounds -> Soundfont Library -> Collections).
-   mingus uses the fluidsynth server to send the MIDI signals. To start a server
-   instance of fluidsynth, use a command like this: 
-   
-   	fluidsynth -is -m alsa_seq ./nameofinstrbank.sf2
-
-   NB. Because fluidsynth uses unix style sockets, this doesn't work on Windows.
-   We are looking for a solution to make mingus truely cross-platform.
+   This module provides a [refMingusExtraMidisequencer MidiSequencer] which will take the objects from 
+   mingus.containers and turn them into MIDI messages.
 
 ================================================================================
 
 """
 
-from telnetlib import Telnet
 from datetime import datetime
-from MidiSequencer import MidiSequencer
+from mingus.containers.Instrument import MidiInstrument
+import fluidsynth as fs
+from time import sleep
+
+class MidiSequencer:
+	"""A simple MidiSequencer for FluidSynth"""
+
+	output = None
 
 
-fluid = None
-midi = None
+	def __init__(self):
+		self.fs = fs.Synth()
 
+	def start_audio_output(self, driver=None):
+		"""The optional driver argument can be any of 'alsa', \
+'oss', 'jack', 'portaudio', 'sndmgr', 'coreaudio', 'Direct Sound'. \
+Not all drivers will be available for every platform."""
+		self.fs.start(driver)
 
-def init(server="localhost", port=9800):
-	"""Initializes a connection to the fluidsynth server. 
-	You have to call this function when you want the rest to work."""
-	global fluid, midi
-	try:
-		fluid = Telnet(server, port)
-		midi = MidiSequencer(fluid.write)
+	def load_sound_font(self, sf2):
+		"""Loads a sound font. This function should be called \
+before your audio can be played, since the instruments are kept in the \
+sf2 file. Retuns True on success, False on failure."""
+		self.sfid = self.fs.sfload(sf2)
+		if self.sfid == -1:
+			return False
 		return True
-	except:
-		return False
 
 
-def init_fluidsynth(server="localhost", port=9800):
-	"""Same as init(). Held for bugwards compatibility."""
-	return init(server, port)
+	def set_instrument(self, channel, instr):
+		"""Sets the channel to the instrument _instr_."""
+		self.fs.program_select(channel, self.sfid, 0, instr)
+		return True
+
+	def control_change(self, channel, control, value):
+		if control < 0 or control > 128:
+			return False
+		if value < 0 or value > 128:
+			return False
+		self.fs.cc(channel, control, value)
+		return True
 
 
-def play_Note(note, velocity = 100, channel = 1):
-	"""Sends a Note object as midi signal to the fluidsynth server."""
+	def play_Note(self, note, channel = 1, velocity = 100):
+		"""Plays a Note object on a channel[1-16] with a \
+velocity[0-127]. You can either specify the velocity and channel \
+here as arguments or you can set the Note.velocity and Note.channel \
+attributes, which will take presedence over the function arguments."""
+		if hasattr(note, 'velocity'):
+			velocity = note.velocity
+		if hasattr(note, 'channel'):
+			channel = note.channel
+		self.fs.noteon(channel, int(note) + 12, velocity)
+		return True
+
+	def stop_Note(self, note, channel = 1):
+		"""Stops a note on a channel."""
+		return self.fs.noteoff(channel, int(note) + 12)
+
+
+
+
+	def stop_everything(self):
+		"""Stops all the notes on all channels."""
+		for x in range(118):
+			for c in range(16):
+				self.stop_Note(x, c)
+
+
+	def play_NoteContainer(self, nc, channel = 1, velocity = 100):
+		"""Plays the Notes in the NoteContainer nc."""
+		for note in nc:
+			if not self.play_Note(note, channel, velocity):
+				return False
+		return True
+
+
+
+	def stop_NoteContainer(self, nc, channel = 1):
+		"""Stops playing the notes in NoteContainer nc."""
+		for note in nc:
+			if not self.stop_Note(note, channel):
+				return False
+		return True
+
+
+
+	def play_Bar(self, bar, channel = 1, bpm = 120):
+		"""Plays a Bar object. If the channel attribute on a Note\in the Bar has been set it will take presedence over the one you can \
+add here."""
+
+		# length of a quarter note
+		qn_length = 60.0 / bpm
+
+		for nc in bar:
+
+			if not self.play_NoteContainer(nc[2], channel, 100):
+				return False
+			
+			sleep(qn_length * (4.0 / nc[1]))
+			self.stop_NoteContainer(nc[2], channel)
+
+		return True
+
+
+	def play_Bars(self, bars, channels, bpm = 120):
+		"""Plays several bars (a list of Bar objects) at the same time. A list of \
+channels should also be provided."""
+
+		duration = 60.0 / bpm * 4
+		tick = 0.0  # place in beat from 0.0 to bar.length
+		cur = []    # keeps the index of the note needing investigation in each of bars
+		playing = [] # keeps track of the notecontainers being played right now.
+
+
+		# Prepare cur list
+		for x in bars:
+			cur.append(0)
+
+		n = datetime.now()
+		
+		while tick < bars[0].length:
+
+			# Check each bar in bars and investigate index in cur.
+			for x in range(len(bars)):
+
+				bar = bars[x]
+				current_nc = bar[cur[x]]
+
+				# Should note be played?
+				if current_nc[0] <= tick and \
+					current_nc[0] + \
+					(1.0 / current_nc[1]) >= tick \
+					and [current_nc[0], current_nc[1], current_nc[2],\
+						channels[x]] not in playing:
+
+					self.play_NoteContainer(current_nc[2], channels[x])
+					playing.append([current_nc[0], current_nc[1],\
+							current_nc[2], channels[x]])
+					if cur[x] != len(bar) - 1:
+						cur[x] += 1
+
+			# Should any notes stop playing?
+			for p in playing:
+				if p[0] + (1.0 / p[1]) <= tick:
+					self.stop_NoteContainer(p[2], p[3])
+					playing.remove(p)
+
+			
+
+			# Milliseconds so far
+			a = datetime.now()
+			millis = (a - n).microseconds / 1000.0 + (a - n).seconds * 1000.0
+
+			# Calculate new tick
+			tick = (millis / duration) * bars[0].length
+			
+		# Stop all the notes that are still playing
+		for p in playing:
+			self.stop_NoteContainer(p[2], p[3])
+			playing.remove(p)
+
+		return True
+
+
+	def play_Track(self, track, channel = 1):
+		"""Plays a Track object."""
+		for bar in track:
+			# bpm attribute needed? Or just another argument?
+			if not self.play_Bar(bar, channel, 2000):
+				return False
+		return True
+
+	def play_Tracks(self, tracks, channels, keep_playing_func = True):
+		"""Plays a list of Tracks. keep_playing_func can be used to pass a function, \
+which will determine if the tracks should keep playing after each played bar."""
+		
+		# Set the right instruments
+		for x in range(len(tracks)):
+			instr = tracks[x].instrument
+			if isinstance(instr, MidiInstrument):
+				try: 
+					i = instr.names.index(instr.name)
+				except:
+					i = 1
+				self.set_instrument(channels[x], i)
+			else:
+				self.set_instrument(channels[x], 1)
+
+		
+		current_bar = 0
+		max_bar = len(tracks[0])
+
+		# Play the bars
+		while keep_playing_func and current_bar < max_bar:
+			playbars = []
+			for tr in tracks:
+				playbars.append(tr[current_bar])
+			if not self.play_Bars(playbars, channels):
+				return False
+			current_bar += 1
+
+		return True
+			
+
+	def play_Composition(self, composition, channels = None, keep_playing_func = True):
+		"""Plays a Composition object."""
+
+		if channels == None:
+			channels = map(lambda x: x + 1, range(len(composition.tracks)))
+		return self.play_Tracks(composition.tracks, channels, keep_playing_func)
+
+
+	## MIDI CONTROLLER TYPE 'SHORTCUTS'
+
+	def modulation(self, channel, value):
+		"""Sets the modulation"""
+		return self.control_change(channel, 1, value)
+
+	def main_volume(self, channel, value):
+		"""Sets the main volume."""
+		return self.control_change(channel, 7, value)
+
+	def pan(self, channel, value):
+		"""Sets the panning."""
+		return self.control_change(channel, 10, value)
+
+
+midi = MidiSequencer()
+
+def init(sf2):
+	"""This function needs to be called before you can have any \
+audio. The sf2 argument should be the location of a valid soundfont 
+file. Returns True on success, False on failure."""
+	global midi
+	midi.start_audio_output()
+	return midi.load_sound_font(sf2)
+
+
+def play_Note(note, channel = 1, velocity = 100):
+	"""Sends a Note object as midi signal to the fluidsynth server. \
+The channel and velocity can be set as Note attributes as well. If that's \
+the case those values take presedence over the ones given here as function \
+arguments.
+{{{
+>>> n = Note("C", 4)
+>>> n.channel = 9
+>>> n.velocity = 50
+>>> FluidSynth.play_Note(n)
+}}}"""
 	return midi.play_Note(note, channel, velocity)
 
 
-def stop_Note(note, channel = 1):
+def stop_Note(note, channel = 1, velocity = 100):
 	"""Stops the Note playing at channel."""
 	return midi.stop_Note(note, channel)
 
 
-def play_NoteContainer(nc, velocity = 100, channel = 1):
+def play_NoteContainer(nc, channel = 1, velocity = 100):
 	"""Plays the Notes in the NoteContainer nc."""
 	return midi.play_NoteContainer(nc, channel, velocity)
 
@@ -79,12 +294,11 @@ def stop_NoteContainer(nc, channel = 1):
 	"""Stops playing the notes in NoteContainer nc."""
 	return midi.stop_NoteContainer(nc, channel)
 
-def play_Bar(bar, duration = 2000, channel = 1):
-	"""Plays a Bar object. The duration is the duration of the whole bar in milliseconds.\
-The default is set to 2000 ms which is good for 120bpm when playing 4/4 bars."""
+def play_Bar(bar, channel = 1, bpm = 120):
+	"""Plays a Bar object."""
 	return midi.play_Bar(bar, channel, duration)
 
-def play_Bars(bars, channels):
+def play_Bars(bars, channels, bpm = 120):
 	"""Plays a list of bars on the given list of channels."""
 	return midi.play_Bars(bars, channels)
 
@@ -127,3 +341,6 @@ def enable_reverb():
 
 def disable_reverb():
 	return midi.write("reverb off\n")
+
+def set_instrument(channel, instr):
+	return midi.set_instrument(channel, instr)
